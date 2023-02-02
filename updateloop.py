9f968ba539aa062fedb0d72098bd35a87e2dabd1
NexF <lzchen@whu.edu.cn>
2023-02-02 21:26:37 +0800

    优化循环时间计数

--- updateloop.py
+++ updateloop.py
@@ -1,7 +1,7 @@
 import time
 
 from updateobj  import UpdateObj
-from threading  import Thread
+from threading  import Thread, Lock
 from typing     import Dict
 
 class UpdateLoop(Thread):
@@ -12,7 +12,6 @@ class UpdateLoop(Thread):
         self.interval = interval            # 单位 秒
         self.__loop_counts = 0              # 更新次数的计数，有些指标或维度并不需要次次更新
         self.obj_idx = 0                    # 保存loop中存储对象的最大id，用于生成唯一id
-        self.__del_list = []                # 保存等待删除的objidx
         super().__init__()
 
     def add_obj(self, obj : UpdateObj):
@@ -28,7 +27,10 @@ class UpdateLoop(Thread):
         return self.obj_idx - 1
 
     def del_any_idx(self, idx):
-        self.__del_list.append(idx)
+        if idx in self.pure_Update:
+            self.pure_Update.pop(idx)
+        elif idx in self.target_update:
+            self.target_update.pop(idx)
 
 
     def del_any(self, obj : UpdateObj):
@@ -36,35 +38,28 @@ class UpdateLoop(Thread):
 
 
     def run(self) -> None:
+        last_loop_time = time.time()
         while(1):
-            time.sleep(self.interval)
-            self.__loop()
+            if last_loop_time + self.interval <= time.time():
+                last_loop_time = time.time()
+                self.__loop()
         
     def __loop(self):
-        # 看看有没有需要删除的元素，先删除元素
-        for delidx in self.__del_list:
-            if delidx in self.pure_Update:
-                self.pure_Update.pop(delidx)
+        # 处理pure_obj list()防止循环的时候被修改
+        for pure_obj in list(self.pure_Update.values()):
+            if self.__loop_counts % pure_obj.interval != 0:
                 continue
-            elif delidx in self.target_update:
-                self.target_update.pop(delidx)
-                continue
-
-        # 处理pure_obj
-        for pure_obj in self.pure_Update:
-            if self.__loop_counts % self.pure_Update[pure_obj].interval != 0:
-                continue
-
-            if self.pure_Update[pure_obj].update() != 0:
+            
+            if pure_obj.update() != 0:
                 # 错误处理
                 continue
 
         # 再处理target_obj，因为target可能依赖于pureobj
-        for target_obj in self.target_update:
-            if self.__loop_counts % self.target_update[target_obj].interval != 0:
+        for target_obj in list(self.target_update.values()):
+            if self.__loop_counts % target_obj.interval != 0:
                 continue
 
-            if self.target_update[target_obj].update() != 0:
+            if target_obj.update() != 0:
                 # 错误处理
                 continue
 
